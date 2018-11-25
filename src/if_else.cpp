5a40d1f722508569c205c5cbdda030a46f7dff56
Pratik Jain <pratik.jain448@gmail.com>
2018-11-25 16:00:12 +0530

    Completed codegen for if_else block

--- src/if_else.cpp
+++ src/if_else.cpp
@@ -31,7 +31,8 @@ llvm::Value* kif::codegen() {
     if (!condv)
         return nullptr;
     
-    condv = builder.CreateFCmpONE(condv, llvm::ConstantFP::get(the_context, llvm::APFloat(0.0)), "ifcond");
+    // Not required now
+    // condv = builder.CreateFCmpONE(condv, llvm::ConstantFP::get(the_context, llvm::APFloat(0.0)), "ifcond");
 
     llvm::Function* the_func = builder.GetInsertBlock()->getParent();
 
@@ -41,9 +42,44 @@ llvm::Value* kif::codegen() {
     llvm::BasicBlock *after_if = else_bb;
 
     bool if_ret = blk->has_return();
+    bool els_ret = (els_blk) ? els_blk->has_return() : false;
+    if(!els_blk)
+        after_if = next_bb;
     
+    builder.CreateCondBr(condv, if_bb, after_if);
+    builder.SetInsertPoint(if_bb);
 
-    // TODO: Work to be done
+    llvm::Value *if_val = blk->codegen();
+    if(!if_val)
+        return if_val;
+    
+    if (!if_ret)
+        builder.CreateBr(next_bb);
+
+    if_bb = builder.GetInsertBlock();
+
+    llvm::Value* els_val;
+
+    if (els_blk) {
+        the_func->getBasicBlockList().push_back(else_bb);
+        builder.SetInsertPoint(else_bb);
+        els_val = els_blk->codegen();
+        if(!els_val)
+            return els_val;
+        if(els_ret)
+            builder.CreateBr(next_bb);
+    }
+
+    the_func->getBasicBlockList().push_back(next_bb);
+
+    builder.SetInsertPoint(next_bb);
+
+    if(els_ret && if_ret) {
+        llvm::Type* ret_type = builder.GetInsertBlock()->getParent()->getReturnType();
+        // TODO: Add for Void
+        builder.CreateRet(llvm::ConstantInt::get(the_context, llvm::APInt(INT_WIDTH, SUCCESS)));
+    }
+    llvm::Value* ret_val = llvm::ConstantInt::get(the_context, llvm::APInt(INT_WIDTH, SUCCESS));
     return nullptr;
 }
 
